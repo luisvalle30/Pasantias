798847292fb21b8beffb1d31d05a683dcd67a7c0
Luis <valle_luis@hotmail.com>
2018-07-30 17:36:21 -0600

    Subiendo archivos pasantías

---
+++ borrar.php
@@ -0,0 +1,23 @@
+<?php 
+include 'conexion.php';
+
+ $id = $_REQUEST['id'];
+
+ $delete= $con -> query("DELETE FROM alumnos WHERE id = '$id'");
+ if ($delete) {
+ 	# code...
+
+ 	echo "<script>
+	location.href='formulario.php';
+	</script>";
+
+ }else{
+
+ 	echo "<script>
+ 	alert('El registro no se pudo eliminar');
+	location.href='formulario.php';
+	</script>";
+
+ }
+
+ ?>
---
+++ ejemplo.php
@@ -0,0 +1,24 @@
+<?php
+
+//$frutas=array('peras', 'manzanas', 'nepes', 'uvas');
+
+
+
+/*
+if ($frutas[1]=='nepes') {
+	# code...
+	echo "exito prro";
+}else{
+
+	echo "Nel prro";
+}
+*/
+
+$frutas2= array('clave1' => 'peras', 'clave 2'=>'manzanas');
+
+$frutas=array('peras', 'manzanas', 
+	'pn' => 'nepes', 'uvas');
+
+echo $frutas['pn'];
+
+?>
